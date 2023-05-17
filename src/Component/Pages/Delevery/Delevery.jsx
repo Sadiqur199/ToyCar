8519f9c691bd6410b2396600f07b130809556a2f
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-05-18 05:27:05 +0600

    delivery system update

--- src/Component/Pages/Delevery/Delevery.jsx
+++ src/Component/Pages/Delevery/Delevery.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import img1 from '../../../assets/paperfly.png'
-import img2 from '../../../assets/phatao.png'
+import img2 from '../../../assets/streetFast.png'
 import img4 from '../../../assets/sundarban.jpg'
 const Delevery = () => {
   return (
     <div>
-      <div>
-        <h1>Our Delivery System</h1>
+      <div className='mt-24 mb-16 text-center'>
+        <h1 className='font-bold text-3xl'>Our Delivery System</h1>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 border mb-16 ml-16'>
 
-        <div className="hero  h-[150px]  bg-base-200">
+        <div className="hero  md:h-[150px] m-2  bg-base-200">
           <div className="hero-content flex-col lg:flex-row">
             <img src={img1} className="max-w-sm rounded w-1/3 h-1/3 shadow-2xl" />
             <div>
-              <h1 className="font-bold">Box Office News!</h1>
+              <h1 className="font-bold">PaperFly!</h1>
+              <button className='btn btn-outline mt-2'>Confirm</button>
             </div>
           </div>
         </div>
 
-        <div className="hero  h-[150px] bg-base-200">
+        <div className="hero  md:h-[150px] m-2 rounded  bg-base-200">
           <div className="hero-content flex-col lg:flex-row">
             <img src={img2} className="max-w-sm rounded w-1/3 h-1/3 shadow-2xl" />
             <div>
-              <h1 className="font-bold">Box Office News!</h1>
+              <h1 className="font-bold">StreetFast</h1>
+              <button className='btn btn-outline mt-2'>Confirm</button>
             </div>
           </div>
         </div>
 
-        <div className="hero  h-[150px] bg-base-200">
+        <div className="hero  md:h-[150px] m-2 rounded bg-base-200">
           <div className="hero-content flex-col lg:flex-row">
             <img src={img4} className="max-w-sm rounded w-1/3 h-1/3 shadow-2xl" />
             <div>
-              <h1 className="font-bold">Box Office News!</h1>
+              <h1 className="font-bold">Sundarban</h1>
+              <button className='btn btn-outline mt-2'>Confirm</button>
             </div>
           </div>
         </div>
