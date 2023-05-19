5ab459b3e3e9ac3a27a03d59c1cb5375c120c453
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-05-20 05:01:10 +0600

    updated my toy

--- src/Component/Pages/MyToys/MyToysUpdate.jsx
+++ src/Component/Pages/MyToys/MyToysUpdate.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyToysUpdate = () => {
   const myToyUpdated = useLoaderData()
-  const {name,price,quantity,details} = myToyUpdated;
+  const {_id,name,price,quantity,details} = myToyUpdated;
   const handelUpdateToy =(event)=>{
    event.preventDefault()
    const form = event.target
@@ -13,10 +14,26 @@ const MyToysUpdate = () => {
    const details = form.details.value
    
    const updatedMyToys = {name,price,quantity,details}
-  //  fetch(`http://localhost:5000/addToy`,{
-
-  //  })
-  //  .then
+   fetch(`http://localhost:5000/addToy/${_id}`,{
+    method:'PUT',
+    headers:{
+      'content-type':'application/json'
+    },
+    body:JSON.stringify(updatedMyToys)
+   })
+   .then(res=>res.json())
+   .then(data=>{
+    console.log(data)
+    if(data.modifiedCount>0){
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Toy Update SuccesFUll',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+   })
   }
   return (
     <div>
@@ -29,7 +46,7 @@ const MyToysUpdate = () => {
               <span className="label-text font-bold">ToysName:</span>
             </label>
             <label className="input-group">
-              <input type="text" name="name" defaultValue={name} placeholder="Coffee Name" className="input input-bordered w-full" />
+              <input type="text" name="name" defaultValue={name} placeholder="" className="input input-bordered w-full" />
             </label>
           </div>
         <div className="form-control md:w-1/2 ml-4">
@@ -37,7 +54,7 @@ const MyToysUpdate = () => {
               <span className="label-text font-bold">ToysPrice:</span>
             </label>
             <label className="input-group">
-              <input type="text" name="price" defaultValue={price} placeholder="Coffee Name" className="input input-bordered w-full" />
+              <input type="text" name="price" defaultValue={price} placeholder="" className="input input-bordered w-full" />
             </label>
           </div>
         <div className="form-control md:w-1/2 ml-4">
@@ -45,7 +62,7 @@ const MyToysUpdate = () => {
               <span className="label-text font-bold">ToysQuantity:</span>
             </label>
             <label className="input-group">
-              <input type="text" name="quantity" defaultValue={quantity} placeholder="Coffee Name" className="input input-bordered w-full" />
+              <input type="text" name="quantity" defaultValue={quantity} placeholder="" className="input input-bordered w-full" />
             </label>
           </div>
         <div className="form-control md:w-1/2 ml-4">
@@ -53,7 +70,7 @@ const MyToysUpdate = () => {
               <span className="label-text font-bold">ToysDetails:</span>
             </label>
             <label className="input-group">
-              <input type="text" name="details" defaultValue={details} placeholder="Coffee Name" className="input input-bordered w-full" />
+              <input type="text" name="details" defaultValue={details} placeholder="" className="input input-bordered w-full" />
             </label>
           </div>
           <input type="submit" value="Updated Toy" className="btn pl-20 pr-20  mt-5 ml-4"/>
