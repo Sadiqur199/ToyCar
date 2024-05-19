e9b73f671dff5b812bfd0859c70d036024f18222
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-05-20 05:18:32 +0600

    solve the search functionality

--- src/Component/Pages/AllToys/Alltoys.jsx
+++ src/Component/Pages/AllToys/Alltoys.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import AllToysTable from './AllToysTable';
 import { ToastContainer, toast } from 'react-toastify';
@@ -6,40 +6,44 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AuthContext } from '../../../Provider/AuthProvider';
 
 const Alltoys = () => {
-  const allToys = useLoaderData()
-  const {user} = useContext(AuthContext)
+  const allToys = useLoaderData();
+  const { user } = useContext(AuthContext);
 
-  const [Search , setSearch]  = useState('')
-//search button
-  const handelChange = (event)=>{
+  const [search, setSearch] = useState('');
+  const [displayedToys, setDisplayedToys] = useState(allToys);
+
+  useEffect(() => {
+    const filteredToys = allToys.filter(toy =>
+      toy.name.toLowerCase().includes(search.toLowerCase()) || toy.price.toString().includes(search)
+    );
+    setDisplayedToys(filteredToys);
+  }, [search, allToys]);
+
+  const handleChange = (event) => {
     event.preventDefault();
-    setSearch(event.target.value)
-    setToys(Search);
-    setDisplayedToys(Search.slice(0, 20))
+    setSearch(event.target.value);
+  };
+
+  if (!user) {
+    toast('Please Login This Site');
   }
-  console.log(Search)
-
-  const SearchToy = allToys.filter(ToySearch=>{
-    console.log(ToySearch.name)
-    ToySearch.name.includes(Search)
-
-  })
-
-  if(!user){
-    toast('Please Login This Site')
-  }
-
-
 
   return (
     <>
-      <div>
-      <input type="text" onChange={handelChange}  value={Search}  placeholder="Search your toy" className="input input-bordered mb-10 mt-10 mr-24 w-1/2" />
+      <div className='pl-10'>
+        <label className='font-bold text-xl mr-2'>Search:</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={search}
+          placeholder="Search your toy"
+          className="input input-bordered mb-10 mt-10 mr-24 w-1/2"
+        />
       </div>
       <div className="overflow-x-auto w-full p-10">
-        <table className="table w-full ">
+        <table className="table w-full">
           <thead>
-            <tr >
+            <tr>
               <th className='bg-[#74C4F7]'>Seller Name</th>
               <th className='bg-[#74C4F7]'>Toy Name</th>
               <th className='bg-[#74C4F7]'>Sub-category</th>
@@ -49,18 +53,14 @@ const Alltoys = () => {
             </tr>
           </thead>
           <tbody>
-            {
-              allToys.map(allToy => <AllToysTable
-                key={allToy._id}
-                allToy={allToy}
-              ></AllToysTable>)
-            }
+            {displayedToys.map(allToy => (
+              <AllToysTable key={allToy._id} allToy={allToy} />
+            ))}
           </tbody>
-
         </table>
       </div>
+      <ToastContainer />
     </>
-
   );
 };
 
